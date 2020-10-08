930d8998baa617ff5231a5e8d5ee279114910d9c
Ee-Chee <eechee.leng@gmail.com>
2020-10-09 00:33:57 +0200

    debugging

--- app/controllers/userInfo.controller.js
+++ app/controllers/userInfo.controller.js
@@ -27,7 +27,7 @@ exports.authUser = (req, res) => {
                     if (correctPW) {
                         console.log("datalala", data.id);
                         req.session.userId = data.id;
-                        res.send({ "userId": data.id });
+                        res.status(200).send({ "userId": data.id });
                         // res.status(200).json({
                         //     message: "update completed"
                         // });

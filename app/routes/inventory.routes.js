b4f6e0ff6aa7444f04ccbe8cb505b9ef3d40a385
Ee-Chee <eechee.leng@gmail.com>
2020-10-13 19:43:12 +0200

    debugging

--- app/routes/inventory.routes.js
+++ app/routes/inventory.routes.js
@@ -7,14 +7,14 @@ module.exports = app => {
     router.post("/registration", userInfo.addUser);
 
     router.get("/welcome", (req, res) => {
-        // console.log("testb1", req.session.userId);
+        console.log("testb1", req.session.userId);
         res.send({ userId: req.session.userId });
 
     });
 
     router.get("/goodbye", (req, res) => {
         req.session = null;
-        res.send({msg: "Cookies clear"});
+        res.send({ msg: "Cookies clear" });
     });
 
     router.post("/authentication", userInfo.authUser);
@@ -22,11 +22,11 @@ module.exports = app => {
     router.post("/initialization", inventory.initializeQuantity);
 
     router.get("/amount", inventory.findQuantity);
-//////////////////////////////////////////
+    //////////////////////////////////////////
     router.get("/amountid5", inventory.findQuantityAtUserId5);
 
     router.post("/changeAmountid5", inventory.updateQuantityAtUserId5);
-///////////////////////////////////////////////
+    ///////////////////////////////////////////////
     router.post("/changeAmount", inventory.updateQuantity);
 
     router.get("/goods", inventory.findAllGoods);

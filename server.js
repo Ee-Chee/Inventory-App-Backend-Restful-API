3464c770206d6c970ae3d88f0d32ec6147be6514
Ee-Chee <eechee.leng@gmail.com>
2020-10-13 20:17:51 +0200

    debugging

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cookieSession = require("cookie-session");
 
 var corsOptions = {
     origin: "https://eat-happy-inventur.herokuapp.com",
-    credentials: true
+    withCredentials: true
 };
 
 app.use(cors(corsOptions));

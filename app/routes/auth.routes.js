3c4aa763b44b53cd41dce35a3a40b3cfae2a98d4
Ee-Chee <eechee.leng@gmail.com>
2020-11-01 19:41:21 +0100

    Use jwt

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,14 +1,14 @@
 module.exports = function (app) {
     const userInfo = require("../controllers/userInfo.controller.js");
 
-    app.use(function (req, res, next) {
-        // res.header("Access-Control-Allow-Origin", "http://localhost:8081");
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
-        next();
-    });
+    // app.use(function (req, res, next) {
+    //     // res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+    //     res.header(
+    //         "Access-Control-Allow-Headers",
+    //         "x-access-token, Origin, Content-Type, Accept"
+    //     );
+    //     next();
+    // });
 
     app.post("/api/auth/signup", userInfo.addUser);
 

34c5522af93e45b5cfd99c50e1b4b06e2192d7b9
Ee-Chee <eechee.leng@gmail.com>
2021-03-20 17:26:37 +0100

    remove depreceted body paser

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 // const cookieSession = require("cookie-session");
@@ -30,9 +29,9 @@ app.use(cors(corsOptions));
 
 // app.use(cookieSessionMiddleware);
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 db.sequelize.sync();

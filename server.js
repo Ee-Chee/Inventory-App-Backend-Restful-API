a07be5c63adbad6017ee0dd8fb5ed2e0fc8dcafc
Ee-Chee <eechee.leng@gmail.com>
2021-03-20 21:22:43 +0100

    fix cors issue

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 db.sequelize.sync();

bbba7ebadecf675b9a470ea99a0e4f85b398b03b
Ee-Chee <eechee.leng@gmail.com>
2021-03-20 18:21:36 +0100

    remove depreceted body paser

--- app/routes/inventory.routes.js
+++ app/routes/inventory.routes.js
@@ -3,6 +3,7 @@ module.exports = app => {
     const authJwt = require("../middleware/authJwt");
 
     app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
         res.header(
             "Access-Control-Allow-Headers",
             "x-access-token, Origin, Content-Type, Accept"

c2af3853e9b2a9e424e24718a51aaf84774f3a8a
Ee-Chee <eechee.leng@gmail.com>
2021-03-20 19:43:52 +0100

    fix cors issue

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,13 +1,13 @@
 module.exports = function (app) {
     const userInfo = require("../controllers/userInfo.controller.js");
 
-    // app.use(function (req, res, next) {
-    //     res.header(
-    //         "Access-Control-Allow-Headers",
-    //         "x-access-token, Origin, Content-Type, Accept"
-    //     );
-    //     next();
-    // });
+    app.use(function (req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
 
     app.post("/api/auth/signup", userInfo.addUser);
 
